0840ee0f661bbe7360acbc1c49235f01237310c0
Yashwanth1119 <yashwanthtvv@gmail.com>
2023-09-16 21:26:46 +0530

    model evaluation added

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
-from pathlib import Path 
+from pathlib import Path
 
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
-    root_dir:Path
+    root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
 
 
 @dataclass(frozen=True)
@@ -17,10 +18,13 @@ class DataValidationConfig:
     unzip_data_dir: Path
     all_schema: dict
 
+
+
 @dataclass(frozen=True)
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path
+
 
 
 @dataclass(frozen=True)
@@ -32,3 +36,16 @@ class ModelTrainerConfig:
     alpha: float
     l1_ratio: float
     target_column: str
+
+
+
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    test_data_path: Path
+    model_path: Path
+    all_params: dict
+    metric_file_name: Path
+    target_column: str
+    mlflow_uri: str
